4c4eb2db5b0afdd1b06db48e87d207b4b9452020
DESKTOP-EL58LL7\ixtla <tstecker@student.cscc.edu>
2018-12-21 20:01:16 -0500

    flexdirection: column

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class FlexDirectionBasics extends Component {
   render() {
     return (
       // Try setting 'flexDirection' to ' column'.
-      <View style={{flex: 1, flexDirection: 'row'}}>
+      <View style={{flex: 1, flexDirection: 'column'}}>
         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
         <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
         <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
