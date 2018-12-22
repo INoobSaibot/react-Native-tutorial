98eb58cb3ab8b3476b78be618a0176a5e173f554
DESKTOP-EL58LL7\ixtla <tstecker@student.cscc.edu>
2018-12-21 19:58:50 -0500

    felx direction: row

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
 
-export default class FixedDimensions extends Component {
+export default class FlexDirectionBasics extends Component {
   render() {
     return (
-      <View>
+      // Try setting 'flexDirection' to ' column'.
+      <View style={{flex: 1, flexDirection: 'row'}}>
         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
       </View>
+
     );
   }
 }
